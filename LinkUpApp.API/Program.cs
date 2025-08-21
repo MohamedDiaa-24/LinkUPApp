ee1bd4eca5decad4cc652e371639c6011178daf6
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-08-21 12:53:28 +0300

    Add cors in program.cs for angular client

--- LinkUpApp.API/Program.cs
+++ LinkUpApp.API/Program.cs
@@ -6,12 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(options=>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
+app.UseCors(config=>
+{
+    config.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200");
+});
 
 app.MapControllers();
 
