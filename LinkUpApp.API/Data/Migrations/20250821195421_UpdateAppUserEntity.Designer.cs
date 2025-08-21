bc1e342b360329e2ed885341aac55b5113216504
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-08-21 23:08:00 +0300

    Update user Entity

--- LinkUpApp.API/Data/Migrations/20250821195421_UpdateAppUserEntity.Designer.cs
+++ LinkUpApp.API/Data/Migrations/20250821195421_UpdateAppUserEntity.Designer.cs
@@ -2,6 +2,7 @@
 using LinkUpApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LinkUpApp.API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250821195421_UpdateAppUserEntity")]
+    partial class UpdateAppUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
