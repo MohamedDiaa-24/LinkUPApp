09f05a5931bce3f2fe3182def782d9c9c552a09a
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-08-24 16:36:34 +0300

    Add account services

---
+++ linkup-client/src/app/services/account.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../models/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  private baseUrl = 'https://localhost:5001/api/';
+  currentUser = signal<User | null>(null);
+
+  login(model: any) {
+    return this.http.post<User>(`${this.baseUrl}account/login`, model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  Logout(){
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
